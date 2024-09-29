f89e52df84a19d147b4852f4730de0f72505045a
basswoman <54603761+basswoman@users.noreply.github.com>
2024-09-29 17:44:43 -0400

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { createRoot } from 'react-dom/client';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Details from './Details';
 import SearchParams from './SearchParams';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      {/* <Pet name="Luna" breed="Japanese" animal="dog" /> */}
-      {/* <Pet name="Pepper" breed="Cockatiel" animal="bird" /> */}
-      {/* <Pet name="Dink" breed="Mixed" animal="cat" /> */}
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,10 +1,23 @@
-const Pet = props => {
+import { Link } from 'react-router-dom';
+
+const Pet = ({ name, animal, breed, location, id, images }) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h1>Name: {props.name}</h1>
-      <h2>Animal: {props.animal}</h2>
-      <h2>Breed: {props.breed}</h2>
-    </div>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </Link>
   );
 };
 
